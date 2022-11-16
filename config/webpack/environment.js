afad4ac352c215268d7f90b2d8e79e90bb4a9766
Josh Wood <josh@joshuawood.net>
2022-11-16 10:39:26 -0800

    install @honeybadger-io/webpack per instructions
    I had to move the package out of devDependencies in package.json for
    webpacker to find it.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
 const { environment } = require('@rails/webpacker')
+const HoneybadgerSourceMapPlugin = require('@honeybadger-io/webpack')
+
+// Assumes Heroku / 12-factor application style ENV variables
+// named GIT_COMMIT, HONEYBADGER_API_KEY, ASSETS_URL
+const revision = process.env.GIT_COMMIT || 'main'
+
+environment.plugins.append(
+  'HoneybadgerSourceMap',
+  new HoneybadgerSourceMapPlugin({
+    apiKey: process.env.HONEYBADGER_API_KEY,
+    assetsUrl: "https://www.example.com/assets",
+    silent: false,
+    ignoreErrors: false,
+    revision: revision
+  }))
 
 module.exports = environment
